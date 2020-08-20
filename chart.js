8ebd6b5dfe2168f9c47e36897d574deb9a777f86
Abdullahriyd <Abdullahriyd>
2020-08-20 15:12:40 +0600

    Bar & Line Mixed Chart

---
+++ chart.js
@@ -0,0 +1,45 @@
+let chartArea = document.getElementById('my-chart').getContext("2d");
+
+let chart = new Chart (chartArea, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Number of Votes',
+            data: [10, 19, 3, 15, 2, 7],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        },
+        {
+            label: ['Shadow Wisher'],
+            data: [17, 11, 33, 14,8,29],
+
+            // Changes this dataset to become a line
+            type: 'line'
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+})
